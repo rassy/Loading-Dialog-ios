2b0705a96fbd4c646cea95d9e2c5eaf550efdad8
Tomohiro Ryumura <ryumura@gmail.com>
2013-02-12 22:58:01 +0900

    first commit

--- LoadingDialog/ViewController.m
+++ LoadingDialog/ViewController.m
@@ -17,13 +17,28 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // show UIAlertView
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Please wait..." message:nil delegate:nil cancelButtonTitle:nil otherButtonTitles:nil, nil];
+    [alertView show];
+    
+    // show UIActivityIndicatorView
+    UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    indicator.center = CGPointMake(alertView.bounds.size.width * 0.5f, alertView.bounds.size.height * 0.5f +10);
+    [indicator startAnimating];
+    [alertView addSubview:indicator];
+
+    // close after 2 seconds.
+    double delayInSeconds = 2.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [alertView dismissWithClickedButtonIndex:0 animated:NO];
+    });
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
